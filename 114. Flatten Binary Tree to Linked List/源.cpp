749a30bd82b07c3f7664ed743476c98539f1e34b
liuce- <lc453297223@qq.com>
2018-06-27 11:57:02 +0800

    New Problem Solved

--- 114. Flatten Binary Tree to Linked List/源.cpp
+++ 114. Flatten Binary Tree to Linked List/源.cpp
@@ -28,27 +28,59 @@
      TreeNode *right;
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  };
-
+ //o(n) n=total nodes
  class Solution {
 	 TreeNode* f(TreeNode* root) {
-		 TreeNode* end=nullptr;
-		 if (root->left==nullptr && root->right!=nullptr) {
-			 end = f(root->right);
-		 }
-		 else if (root->left != nullptr&&root->right == nullptr) {
-			 end = f(root->left);
+		 if (root == nullptr)
+			 return nullptr;
+		 TreeNode* leftEnd = f(root->left);
+		 TreeNode* rightEnd = f(root->right);
+		 if (leftEnd == nullptr&&rightEnd == nullptr)
+			 return root;
+		 else if (leftEnd != nullptr&&rightEnd == nullptr) {
 			 root->right = root->left;
 			 root->left = nullptr;
+			 return leftEnd;
 		 }
-		 else {
-			 end = f(root->left);
-
+		 else if (leftEnd == nullptr&&rightEnd != nullptr)
+			 return rightEnd;
+		 else {//leftEnd!=nullptr && rightEnd!=nullptr
+			 leftEnd->right = root->right;
+			 root->right = root->left;
+			 root->left = nullptr;
+			 return rightEnd;
 		 }
 
 	 }
  public:
 	 void flatten(TreeNode* root) {
-		 if (root != nullptr)
-			 f(root);
+		 f(root);
 	 }
  };
+ //source :https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/37010/Share-my-simple-NON-recursive-solution-O(1)-space-complexity!
+ //o(n) n=total nodes
+ class Solution {
+ public:
+	 void flatten(TreeNode *root) {
+		 TreeNode*now = root;
+		 while (now)
+		 {
+			 if (now->left)
+			 {
+				 //Find current node's prenode that links to current node's right subtree
+				 TreeNode* pre = now->left;
+				 while (pre->right)
+				 {
+					 pre = pre->right;
+				 }
+				 pre->right = now->right;
+				 //Use current node's left subtree to replace its right subtree(original right 
+				 //subtree is already linked by current node's prenode
+				 now->right = now->left;
+				 now->left = nullptr;
+			 }
+			 now = now->right;
+		 }
+	 }
+ };
+
