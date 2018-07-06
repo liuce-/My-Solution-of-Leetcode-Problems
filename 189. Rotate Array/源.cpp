f8644cea7d64f92a72da8fbc6e71a8127b8382eb
liuce- <lc453297223@qq.com>
2018-07-06 22:48:02 +0800

    New Problem Solved

---
+++ 189. Rotate Array/源.cpp
@@ -0,0 +1,48 @@
+//Given an array, rotate the array to the right by k steps, where k is non - negative.
+//
+//Example 1:
+//
+//Input: [1, 2, 3, 4, 5, 6, 7] and k = 3
+//	Output : [5, 6, 7, 1, 2, 3, 4]
+//	Explanation :
+//	rotate 1 steps to the right : [7, 1, 2, 3, 4, 5, 6]
+//	rotate 2 steps to the right : [6, 7, 1, 2, 3, 4, 5]
+//	rotate 3 steps to the right : [5, 6, 7, 1, 2, 3, 4]
+//	Example 2 :
+//
+//	Input : [-1, -100, 3, 99] and k = 2
+//	Output : [3, 99, -1, -100]
+//	Explanation :
+//	rotate 1 steps to the right : [99, -1, -100, 3]
+//	rotate 2 steps to the right : [3, 99, -1, -100]
+//	Note :
+//
+//	Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+//	Could you do it in - place with O(1) extra space ?
+#include<vector>
+#include<numeric>
+using namespace std;
+//O(N) time, O(1) space£¬ beats 100%
+class Solution {
+	int GCD(int a, int b) {
+
+		if (b == 0) return a;
+		else return GCD(b, a%b);
+	}
+
+public:
+	void rotate(vector<int>& nums, int k) {
+		if (nums.size() != 0 && k!=0) {
+			int count = GCD(nums.size(), k);
+			for (int i = 0; i < count; i++) {
+				int tmp = nums[i];
+				int index = (i+k) % nums.size();
+				while (index != i) {
+					swap(tmp, nums[index]);
+					index = (index + k) % nums.size();
+				}
+				swap(tmp, nums[i]);
+			}
+		}
+	}
+};
---
+++ 190. Reverse Bits/源.cpp
@@ -0,0 +1,16 @@
+//Reverse bits of a given 32 bits unsigned integer.
+//
+//Example:
+//
+//Input: 43261596
+//	Output : 964176192
+//	Explanation : 43261596 represented in binary as 00000010100101000001111010011100,
+//	return 964176192 ¡¤¡¤ in binary as 00111001011110000010100101000000.
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+
+	}
+};
